7be171332c21f45b7498cb07bcc4d57983f2043f
Youssef Badway <youssefbadway@gmail.com>
2025-04-05 05:35:44 +0200

    first commit

---
+++ app/src/main/java/com/example/kotlinunlimited/kotlinUnlimitedTasks/sudokuChecker/IPv4Checker.kt
@@ -0,0 +1,5 @@
+package com.example.kotlinunlimited.kotlinUnlimitedTasks.sudokuChecker
+
+fun isValidIPv4(address: String): Boolean {
+    return false
+}
---
+++ app/src/test/java/com/example/kotlinunlimited/kotlinUnlimitedTasks/sudokuChecker/IPv4CheckerTest.kt
@@ -0,0 +1,4 @@
+package com.example.kotlinunlimited.kotlinUnlimitedTasks.sudokuChecker
+
+import org.junit.jupiter.api.Assertions.*
+ class IPv4CheckerTest
